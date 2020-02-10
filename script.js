73d26f9b42f80225ff92c1d25e26ff431afd326b
ujjwal63 <60508711+ujjwal63@users.noreply.github.com>
2020-02-10 22:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const linked = document.getElementById("linked")
+const git = document.getElementById("git")
+const progate = document.getElementById("progate")
+
+
+linked.addEventListener("click", (() => {
+    alert("https://www.linkedin.com/in/gupta--ujjwal/")
+}))
+
+git.addEventListener("click", (() => {
+    alert("https://github.com/ujjwal63")
+}))
+
+progate.addEventListener("click", (() => {
+    alert("https://progate.com")
+}))
